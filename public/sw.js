766cf2762e088be57d908764ed34ccd83de2ed59
Ольга Каштанова <olgakastanova48386@gmail.com>
2026-09-07 13:35:30 +0300

    add ALMA service worker

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+const CACHE = "alma-shell-v1";
+const SHELL = ["/", "/surprise", "/map", "/profile", "/manifest.webmanifest", "/alma-icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).catch(() => undefined));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(() => undefined);
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("/")))
+  );
+});
