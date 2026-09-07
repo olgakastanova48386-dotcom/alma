cc9ffcce5f9bf959c445aebbb7feb68d2a7636c0
Ольга Каштанова <olgakastanova48386@gmail.com>
2026-09-07 04:21:38 +0300

    Remove legacy map search enhancer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/Header";
 import DogFriendlyMapEnhancer from "@/components/DogFriendlyMapEnhancer";
-import MapSearchEnhancer from "@/components/MapSearchEnhancer";
 import LegalFooter from "@/components/LegalFooter";
 import "./globals.css";
 
@@ -35,7 +34,6 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Header />
         <DogFriendlyMapEnhancer />
-        <MapSearchEnhancer />
         <div className="flex-1">{children}</div>
         <LegalFooter />
       </body>
