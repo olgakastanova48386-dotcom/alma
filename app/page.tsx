bc2f88e55853f0b09113c2dc66e8417b0a541833
Ольга Каштанова <olgakastanova48386@gmail.com>
2026-09-20 14:19:35 +0300

    Restore Russian Museum photo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import UnifiedMap from "@/components/UnifiedMap";
 
 const places = [
-  { title: "Русский музей", category: "Музей", image: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Russian_Museum_SPB.jpg" },
+  { title: "Русский музей", category: "Музей", image: "https://triptospb.ru/wp-content/uploads/2023/06/Russian_museum_1.jpg" },
   { title: "Петергоф", category: "Музей-заповедник", image: "/place-images/114.jpg" },
   { title: "Государственный Эрмитаж", category: "Музей", image: "/place-images/63.jpg" },
   { title: "Спас на Крови", category: "Архитектура", image: "https://commons.wikimedia.org/wiki/Special:Redirect/file/Church_of_the_Savior_on_Blood_SPB.jpg" },
